51b9451b66fbceabf8a3213bee0b9177813cafb3
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2025-06-14 22:44:00 -0300

    feat: add sortear request

--- app/core.py
+++ app/core.py
@@ -2,5 +2,5 @@ from app.db import SupabaseDB
 
 db = SupabaseDB()
 
-def realizar_sorteio():
+def realizar_sorteio(request):
     return db.get_condominios() # retornando todos condominios para teste
--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
 from app.core import realizar_sorteio
 
 app = FastAPI()
+
+class VagaUnidadeAtribuida(BaseModel):
+    vaga_id: int
+    unidade_id: int
+
+class SorteioRequest(BaseModel):
+    condominio_id: int
+    torre_id: int
+    vagas_atribuidas: List[VagaUnidadeAtribuida]
 
 @app.get("/")
 def root():
     return {"Hello, World!"}
 
 @app.post("/sortear")
-def sortear():
-    resultado = realizar_sorteio()
+def sortear(request: SorteioRequest):
+    resultado = realizar_sorteio(request)
     return {"resultado": resultado}

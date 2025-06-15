455a8e4a80704dc889a059a9984e93c13a4e0803
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2025-06-14 23:38:03 -0300

    feat: add API authentication

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
+import os
 from app.models import SorteioRequest
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
 
 from app.core import realizar_sorteio
 
@@ -10,6 +11,10 @@ def root():
     return {"Hello, World!"}
 
 @app.post("/sortear")
-def sortear(request: SorteioRequest):
+def sortear(request: SorteioRequest, x_api_key: str = Header(None)):
+    api_key_env = os.getenv("API_KEY")
+    print(api_key_env)
+    if x_api_key != api_key_env:
+        raise HTTPException(status_code=401, detail="API key inválida.")
     resultado = realizar_sorteio(request)
     return {"resultado": resultado}

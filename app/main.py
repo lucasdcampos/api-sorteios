0a6b9e0df8e852ba2a41a8c23f20938b45b26ae6
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2025-06-14 23:49:33 -0300

    refactor: remove useless print

--- app/main.py
+++ app/main.py
@@ -13,7 +13,6 @@ def root():
 @app.post("/sortear")
 def sortear(request: SorteioRequest, x_api_key: str = Header(None)):
     api_key_env = os.getenv("API_KEY")
-    print(api_key_env)
     if x_api_key != api_key_env:
         raise HTTPException(status_code=401, detail="API key inválida.")
     resultado = realizar_sorteio(request)
